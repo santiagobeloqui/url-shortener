94e36a51ffd67a19e38c18c5bdf4335a01bed825
santiagobeloqui <beloquisantiago@gmail.com>
2018-08-20 13:25:45 -0300

    Cambios para portfolio

--- app.js
+++ app.js
@@ -4,32 +4,40 @@ const app = express();
 const bodyParser = require('body-parser');
 const createShortUrl = require('./createShortUrl.js');
 const findOriginalUrl = require('./findOriginalUrl.js');
+const swig = require('swig');
+
+app.engine('html', swig.renderFile);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
 
 
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({extended:false}));
 
-app.get('/api/shorturl', (req,res)=>{
-    res.sendFile(__dirname + "/index.html");
+app.get('/', (req,res)=>{
+    // res.sendFile(__dirname + "/index.html");
+    res.render('index');
 });
 
-app.get('/', (req, res)=>{
-    res.redirect('/api/shorturl');
-});
+// app.get('/', (req, res)=>{
+//     res.redirect('/api/shorturl');
+// });
 
-app.post('/api/shorturl/new', (req,res)=>{
+app.post('/new', (req,res)=>{
     let checkUrl = /https?:\/\/(\w+\.)+\w+((\/S+)*)?/;
     if(checkUrl.test(req.body.originalUrl) == true){
         createShortUrl(req.body.originalUrl).then((data)=>{
-            res.json({"original_url": data['original_url'], "short_url": data['short_url']});
+            // res.json({"original_url": data['original_url'], "short_url": data['short_url']});
+            res.render('short', {short: data['short_url']});
+            
         });
     } else{
-        res.json({"error":"invalid URL"});
+        res.render('invalid');
     }
         
 });
 
-app.get('/api/shorturl/:short', (req,res)=>{
+app.get('/:short', (req,res)=>{
     findOriginalUrl(req.params.short).then((data)=>{
         res.redirect(data['original_url']);
     });
